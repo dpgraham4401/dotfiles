3246fb2966f19a24a6c49a6a9c17f1b04509f8cb
David Graham <dpgraham4401@gmail.com>
2024-09-25 11:33:57 -0400

    add more language to our tree-sitter config

--- nvim/.config/nvim/lua/tree-sitter.lua
+++ nvim/.config/nvim/lua/tree-sitter.lua
@@ -1,7 +1,23 @@
 -- Use Lua to confiure tree-sitter
 -- https://github.com/nvim-treesitter/nvim-treesitter
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "javascript", "typescript", "python", "vim", "json", "html", "java", "tsx", "lua" },
+    ensure_installed = { 
+		"javascript", 
+		"typescript",
+		"python",
+		"vim",
+		"json",
+		"html",
+		"java",
+		"tsx",
+		"lua",
+		"c",
+		"cpp",
+		"astro",
+		"toml",
+		"rust",
+		"yaml" 
+	},
     sync_install = false,
     auto_install = true,
     highlight = {
